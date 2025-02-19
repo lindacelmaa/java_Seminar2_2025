a72a548974aa4ba71e4e4ce172bcea0d442ab651
lindacelmaa <lindacelma98@gmail.com>
2025-02-19 12:09:41 +0200

    Incomplete Courses class

---
+++ src/model/Course.java
@@ -0,0 +1,60 @@
+package model;
+
+import model.Professor;
+
+public class Course {
+	private long cID;
+	private String title;
+	private int creditPoints;
+	Professor professor;
+	
+	private static long counter = 100000;
+
+	public long getcID() {
+		return cID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getCreditPoints() {
+		return creditPoints;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setcID(long cID) {
+		cID= counter;
+		counter++;
+	}
+
+	public void setTitle(String inputTitle) {
+		if(inputTitle != null 
+				&& inputTitle.matches("[A-Za-z]{3,20}([ ][A-Za-z]{3,20})?")){
+			title = inputTitle;
+		}else {
+			title = "unknown";
+		}
+	}
+
+	public void setCreditPoints(int inputCreditPoints) {
+		if(inputCreditPoints > 0 && inputCreditPoints <=30)
+		{
+			creditPoints = inputCreditPoints;
+		}else {
+			creditPoints = 3;
+		}
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+	
+	
+	
+	
+	
+}
