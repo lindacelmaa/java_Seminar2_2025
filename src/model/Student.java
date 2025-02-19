5d2990479e04f89d23767276fc5f0b737fa0008a
lindacelmaa <lindacelma98@gmail.com>
2025-02-19 10:32:24 +0200

    Getters and setters in student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Student {
+	//mainigie
+	private long stID;
+	private String name;
+	private String surname;
+	
+	//getters
+	public long getStID() {
+		return stID;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
 
+	
+	//setter
+	
+	public void setStID(long inputStID) {
+		stID= inputStID;
+	}
+
+	public void setName(String inputName) {
+		if(inputName != null && inputName.isEmpty() && inputName.matches("[A-Z]{1}[a-z]{3,10}([ ][A-Z]{1}[a-z]{3,10})?")) {
+			name = inputName;
+		}
+		
+	}
+
+	public void setSurname(String inputSurname) {
+		surname = inputSurname;
+	}
 }
